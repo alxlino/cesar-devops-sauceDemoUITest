ccaa27fc06562d7e0cb86e5c657bc765c11164e5
alxlino <alx.lino@gmail.com>
2024-08-03 15:43:28 -0300

    test: sauceLogin fixed

--- cypress/e2e/sauceLogin.cy.js
+++ cypress/e2e/sauceLogin.cy.js
@@ -9,7 +9,8 @@ describe('Login no Sauce Labs', () => {
 
   it('verificar componentes da lista de produtos', () => {
     cy.get('.title').should('have.text', 'Products');
-    cy.get('[data-test="product_sort_container"]').should('be.visible');
+    // cy.get('[data-test="product_sort_container"]').should('be.visible');
+    cy.get('[data-test="inventory-container"]').should('be.visible');
     cy.get('.shopping_cart_link').should('be.visible');
     cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').should('be.visible');
   })
